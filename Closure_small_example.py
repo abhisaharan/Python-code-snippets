377b358167a3a343bf561c69639814867bebe44c
abhisaharan <abhi.saharan@gmail.com>
2017-08-17 16:30:11 -0700

    Closures updated and new text file about closure added.

--- Closure_small_example.py
+++
@@ -1,9 +0,0 @@
-def outer_func(message):
-
-    def inner_func(msg):
-        print(message)
-        print(msg)
-    return inner_func
-a = outer_func("hii")
-a("hi")  # if you do a.__name__ you will find that "a" is inner_func
-a("hello")
