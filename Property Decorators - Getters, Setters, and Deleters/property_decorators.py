1e592f9d577648bf10909f3afee87c3b863c5358
abhisaharan <Abhishek1!>
2017-09-10 11:21:15 -0700

    Duck typing and EAFP

--- Property Decorators - Getters, Setters, and Deleters/property_decorators.py
+++ Property Decorators - Getters, Setters, and Deleters/property_decorators.py
@@ -18,3 +18,8 @@ emp_1 = Employee('John', 'Smith')
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
+
+class Dev(Employee):
+    pass
+
+print(help(Dev))
--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
-print (bin(1))
-for n in range(1,6):
-    print(bin(n))
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)])
