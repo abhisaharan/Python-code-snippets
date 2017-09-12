75680ffebc30e7f9eadda90e4ae0281fd6c16801
abhisaharan <Abhishek1!>
2017-09-12 01:29:58 -0700

    Exception Handling

--- practice.py
+++ practice.py
@@ -1,9 +1,2 @@
-def median(numbers):
-    new=sorted(numbers)
-    l=len(numbers)
-    if l%2==0:
-        median=(new[int(l/2)]+new[int((l-2)/2)])/2
-    else:
-        median=new[int((l-1)/2)]
-    return median
-print(median([4,5,5,4]))
+l = [1,2,3,4,5,6]
+
