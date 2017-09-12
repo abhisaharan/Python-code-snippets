f2a131d0f04bfeeed3f06a3707b7070aa14aad4f
abhisaharan <abhi.saharan@gmail.com>
2017-09-11 18:55:29 -0700

    Regular expression basic

--- practice.py
+++ practice.py
@@ -1,3 +1,9 @@
-import AbhishekSaharan_assignment1
-
-print(AbhishekSaharan_assignment1.even_numbers([2,3,4,56]))
+def median(numbers):
+    new=sorted(numbers)
+    l=len(numbers)
+    if l%2==0:
+        median=(new[int(l/2)]+new[int((l-2)/2)])/2
+    else:
+        median=new[int((l-1)/2)]
+    return median
+print(median([4,5,5,4]))
