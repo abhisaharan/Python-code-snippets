92ab593c447c30fad8c02e52b3557eeeb07e0a23
abhisaharan <abhi.saharan@gmail.com>
2017-10-23 21:46:05 -0700

    git brief tutorial

--- practice.py
+++ practice.py
@@ -1,11 +1,28 @@
-def fib(n):
-    a = 0
-    b = 1
-    for i in range(n):
-        temp = b
-        b = a+b
-        a = temp
-        #a, b = b, a+b
-        print(a)
+#!/bin/python3
 
-print(fib(10))
+import sys
+
+n = int(input().strip())
+a = []
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    a.append(a_t)
+sum1 = 0
+n = 0
+for l in a:
+    sum1 += l[n]
+    n += 1
+
+sum2 = 0
+m = 0
+new_main = []
+for l in a:
+    new_list = l[-1::-1]
+    new_main.append(new_list)
+for i in new_main:
+    sum2 += i[m]
+    m += 1
+
+
+print (abs(sum1 + sum2))
+
