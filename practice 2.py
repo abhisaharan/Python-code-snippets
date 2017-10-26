27011d5a794f4007aced68473a29603aba3a528d
abhisaharan <abhi.saharan@gmail.com>
2017-10-25 22:58:11 -0700

    git brief tutorial

--- practice 2.py
+++ practice 2.py
@@ -1,15 +1,33 @@
-number = int(input().strip())
+import sys
 
-space = number
-# for n in range(number-1):
-#     print(" ")
-list = []
-list_s = []
+def timeConversion(s):
+    split_st = s.split(sep=":")
+    split_3rd = split_st[2]
+    new_date = []
+    if "PM" in split_3rd:
+        if split_st[0] == "12":
+            new_date.append(split_st[0])
+            new_date.append(split_st[1])
+            new_date.append(split_3rd[:2])
+        else:
+            new_date.append(str(int(split_st[0]) + 12))
+            new_date.append(split_st[1])
+            new_date.append(split_3rd[:2])
 
-for n in range(1, number+1):
-    list_s.append(n*"#")
-pos_s = 0
-for n in range(number-1, -1, -1 ):
-    length_a = number
-    print("{}{}".format(n*" ",list_s[pos_s]))
-    pos_s += 1
+    elif "AM" in split_3rd:
+        if split_st[0] == "12":
+            new_date.append("00")
+            new_date.append(split_st[1])
+            new_date.append(split_3rd[:2])
+        else:
+            new_date.append(split_st[0])
+            new_date.append(split_st[1])
+            new_date.append(split_3rd[:2])
+
+    return  ":".join(new_date)
+
+
+
+s = input().strip()
+result = timeConversion(s)
+print(result)
