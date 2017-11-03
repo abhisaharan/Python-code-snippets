959ee064f3d19d2da45d12a84d44f55af7f44511
abhisaharan <abhi.saharan@gmail.com>
2017-11-02 17:01:08 -0700

    git brief tutorial

--- practice.py
+++ practice.py
@@ -1,5 +1,21 @@
-n = input()
-arr = list(map(int, input().split()))
-a = max(arr )
-b = arr.count(a)
-print(b)
+def gemstones(arr):
+    new_list = []
+    for list_string in arr:
+        for word in list_string:
+            if word in arr[list_string]:
+                new_list
+
+
+
+
+
+
+
+
+n = int(input().strip())
+arr = []
+for arr_i in range(n):
+   arr_t = str(input().strip())
+   arr.append(arr_t)
+result = gemstones(arr)
+print(result)
