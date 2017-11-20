2ce66760faccec7f0f5b855eaf9200f43e587fc6
abhisaharan <abhi.saharan@gmail.com>
2017-11-19 22:11:29 -0600

    git brief tutorial

--- practice 2.py
+++ practice 2.py
@@ -1,3 +1,7 @@
 if __name__ == '__main__':
     s = input()
-    print(s.isal)
+    print(any([n.isalnum() for n in s]))
+    print(any([n.isalpha() for n in s]))
+    print(any([n.isdigit() for n in s]))
+    print(any([n.islower() for n in s]))
+    print(any([n.isupper() for n in s]))
